d48edd8e2cdd0667eb1b5b79d091f336eab562e6
vgmoose <me@vgmoose.com>
2023-06-09 22:27:41 -0400

    drawutils: add filled box

--- src/DrawUtils.hpp
+++ src/DrawUtils.hpp
@@ -66,6 +66,11 @@ CST_Texture* CST_CreateTextureFromSurface(CST_Renderer* renderer, CST_Surface* s
 void CST_SetQualityHint(const char* quality);
 
 void CST_filledCircleRGBA(CST_Renderer* renderer, uint32_t x, uint32_t y, uint32_t radius, uint32_t r, uint32_t g, uint32_t b, uint32_t a);
+void CST_roundedBoxRGBA (
+	SDL_Renderer *renderer,
+	Sint16 x1, Sint16 y1, Sint16 x2, Sint16 y2,
+	Sint16 rad, Uint8 r, Uint8 g, Uint8 b, Uint8 a
+);
 float CST_GetDpiScale();
 void CST_SetWindowSize(CST_Window* renderer, int w, int h);
 void CST_Delay(int time);
