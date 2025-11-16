548432d1cef6474e4a754eb6b608246425933744
vgmoose <me@vgmoose.com>
2025-11-16 12:40:43 -0500

    dropdown placeholder

---
+++ src/DropDown.cpp
@@ -0,0 +1,11 @@
+#include "DropDown.hpp"
+
+DropDown::DropDown(std::map<std::string, std::string> choices, std::function<void(std::string)> action) {
+
+}
+bool DropDown::process(InputEvents* event) {
+	return false;
+}
+void DropDown::render(Element* parent) {
+
+}
