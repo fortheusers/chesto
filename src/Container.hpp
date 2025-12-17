8524825636d99accdb5ba3e4f348557a80716eee
vgmoose <me@vgmoose.com>
2025-12-16 22:54:51 -0500

    Container: ensure createNode calls add for positioning/sizing logic

--- src/Container.hpp
+++ src/Container.hpp
@@ -7,14 +7,21 @@
 
 namespace Chesto {
 
-class Container : public Element
-{
+class Container : public Element {
 public:
-	Container(int layout = 0, int padding = 10);
-	Element* add(std::unique_ptr<Element> elem);
+  Container(int layout = 0, int padding = 10);
+  Element *add(std::unique_ptr<Element> elem);
 
-	int layout = 0;
-	int padding = 10;
+  // override createNode to use Container's add logic instead
+  template <typename T, typename... Args> T *createNode(Args &&...args) {
+    auto ptr = std::make_unique<T>(std::forward<Args>(args)...);
+    T *rawPtr = ptr.get();
+    add(std::move(ptr));
+    return rawPtr;
+  }
+
+  int layout = 0;
+  int padding = 10;
 };
 
 } // namespace Chesto
